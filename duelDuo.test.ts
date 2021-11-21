e3cf4d602ec669e03b582f8d40cb4b580cdd254d
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-20 19:11:54 -0600

    Completed parts 3 and 4

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,20 +1,41 @@
+import { Builder, Capabilities, By } from "selenium-webdriver";
 
-import { Builder, Capabilities, By } from "selenium-webdriver"
+require("chromedriver");
 
-require('chromedriver')
-
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
-})
+  driver.get("http://localhost:3000/");
+});
 
 afterAll(async () => {
-    driver.quit()
-})
+  driver.quit();
+  await driver.sleep(4000);
+});
 
-test('Title shows up when page loads', async () => {
-    const title = await driver.findElement(By.id('title'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-})
+test("Title shows up when page loads", async () => {
+  const title = await driver.findElement(By.id("title"));
+  const displayed = await title.isDisplayed();
+  await driver.sleep(2000);
+  expect(displayed).toBe(true);
+});
+
+test("Draw buttons displays list of Duo Bots", async () => {
+  await driver.findElement(By.id("draw")).click();
+  const choice = await driver.findElement(By.id("choices"));
+  const displayed = await choice.isDisplayed();
+  await driver.sleep(2000);
+  expect(displayed).toBe(true);
+});
+
+test("Add to Duo button displays player choices section", async () => {
+  await driver.findElement(By.id("draw")).click();
+  await driver.sleep(2000);
+  await driver
+    .findElement(By.xpath(`//*[@id="choices"]/div[1]/button`))
+    .click();
+  const player = await driver.findElement(By.id("player-duo"));
+  const displayed = await player.isDisplayed();
+  await driver.sleep(2000);
+  expect(displayed).toBe(true);
+});
--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,12 @@
-const {shuffleArray} = require('./utils')
+const { shuffleArray } = require("./utils");
 
-describe('shuffleArray should', () => {
-    // CODE HERE
-})
+let testArray = ["Jerahmeel", "Jack", "Jane", "Jade"];
+
+describe("shuffleArray should", () => {
+  test("shuffleArray should return an array", () => {
+    expect(shuffleArray(testArray)).toBeInstanceOf(Array);
+  });
+  test("shuffleArray should return an array of same length as argument", () => {
+    expect(shuffleArray(testArray).length).toBe(testArray.length);
+  });
+});
